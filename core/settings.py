94c7f2009c2dc19821f303c35b3001699a241e83
emre <emrky.2018@icloud.com>
2022-02-01 18:07:44 +0300

    ds

--- core/settings.py
+++ core/settings.py
@@ -67,7 +67,9 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': BASE_DIR / 'commerce/build',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'commerce/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
