86779bbb20680456ef018759877d0fe89f2f252e
emre <emrky.2018@icloud.com>
2022-02-01 17:47:52 +0300

    fds

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-_fsgjc&h=5lk9=^gl0_%*pl%pp8tse^by#josorbehj&_)b8@o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://restframeworkecommerce.herokuapp.com/"]
+ALLOWED_HOSTS = ["restframeworkecommerce.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
